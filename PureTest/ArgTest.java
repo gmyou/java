8b6d6a4848ed9288c06950419c19ad239699142d
gmyou <gmyou71@gmail.com>
2017-07-16 02:10:37 +0900

    arg test

---
+++ PureTest/ArgTest.java
@@ -0,0 +1,13 @@
+public class ArgTest
+{
+    public static void main(String [] args)
+    {
+        String names [] = new String[5];
+        for (int x=0; x < args.length; x++)
+            names[x] = args[x];
+        System.out.println(names[2]);
+    }
+}
+/* Output
+null
+*/
