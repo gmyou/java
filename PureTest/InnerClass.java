5d53f72a62d9ef782f0a646c064c6602c5cbb980
gmyou <gmyou71@gmail.com>
2017-07-16 01:21:45 +0900

    inner class

---
+++ PureTest/InnerClass.java
@@ -0,0 +1,25 @@
+import java.lang.System;
+
+class Boo
+{
+    Boo(String s) { }
+    Boo() { }
+}
+class Bar extends Boo
+{
+    Bar() { }
+    Bar(String s) {super(s);}
+
+    void zoo()
+    {
+      Boo f = new Bar() {
+        // 
+      };
+    }
+}
+public class InnerClass {
+    public static void main(String[] args) {
+        Bar b = new Bar();
+        b.zoo();
+    }
+}
