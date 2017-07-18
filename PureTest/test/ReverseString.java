2b0b095c973af9509d5b6d1dafa28ba714821bd9
gmyou <gmyou71@gmail.com>
2017-07-18 18:50:21 +0900

    StringBuffer

--- PureTest/test/ReverseString.java
+++ PureTest/test/ReverseString.java
@@ -4,10 +4,21 @@ public class ReverseString {
 
 	public static void main(String[] args) {
 		String str = "Reverse this strings";
-		
-		for (int i=str.length()-1; i>=0; i--) {
+
+		for (int i = str.length() - 1; i >= 0; i--) {
 			System.out.print(str.charAt(i));
 		}
+
+		System.out.println("");
+		System.out.println("StringBuffer");
+		// 버퍼를 만듭니다.
+		StringBuffer strBuffer = new StringBuffer();
+		// 변환할 문자열
+		str = "Reverse this strings";
+		// 버퍼에 문자열을 넣고
+		strBuffer.append(str);
+		// 버퍼안에 reverse()를 이용해 거꾸로 출력
+		System.out.print(strBuffer.reverse());
 	}
 
 }
